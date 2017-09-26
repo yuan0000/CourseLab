3a97982f871ccfc585697dbd05045fe89852247e
yuan0000 <32304422+yuan0000@users.noreply.github.com>
2017-09-26 23:28:26 +0800

    Add files via upload

---
+++ code/loop.py
@@ -0,0 +1,8 @@
+max_n = 10
+for n in range(2, max_n):
+    for x in range(2, n):
+        if n % x == 0:
+            print n, "equals", x , "*", n / x
+            break
+    else:
+        print n, "is a prime number"
